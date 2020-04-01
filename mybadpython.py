d2457ee22e3d5ff27fdfc913c9bda140593fc889
Christopher Johnston <12930132+chrisj-au@users.noreply.github.com>
2020-04-02 06:09:45 +1000

    Update mybadpython.py

--- mybadpython.py
+++ mybadpython.py
@@ -1,4 +1,19 @@
-is this even valid syntax?
-nup
+# Python program to find the factorial of a number provided by the user.
 
-morestuff()
+# change the value for a different result
+num = 7
+
+# To take input from the user
+#num = int(input("Enter a number: "))
+
+factorial = 1
+
+# check if the number is negative, positive or zero
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print("The factorial of",num,"is",factorial)
